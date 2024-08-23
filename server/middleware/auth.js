1b3860fc30cb2417807e5ea7e6ceac775b6e531c
Mirzam Avicena <avicenamirzam@gmail.com>
2024-08-24 00:10:59 +0700

    server: setup feature middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+	const token = req.headers["authorization"]?.split(" ")[1];
+	if (!token) return res.status(401).json({ message: "No token provided" });
+
+	jwt.verify(token, "SECRET_KEY", (err, decoded) => {
+		if (err) return res.status(401).json({ message: "Invalid token" });
+		req.userId = decoded.userId;
+		next();
+	});
+};
